9c70597dd123fb9a96430598cf39a275744ca2a2
Dari <dariia.kulikova@edu.uekat.pl>
2022-06-12 16:53:45 +0200

    icon

--- src/index.js
+++ src/index.js
@@ -22,20 +22,35 @@ function formatDate(date) {
   
     return `${day} ${hours}:${minutes}`;
   }
+
+
+  function displayTemperature(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
   
-  function displayWeatherCondition(response) {
-    document.querySelector("#city").innerHTML = response.data.name;
-    document.querySelector("#temperature").innerHTML = Math.round(
-      response.data.main.temp
-    );
+    celsiusTemperature = response.data.main.temp;
   
-    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-    document.querySelector("#wind").innerHTML = Math.round(
-      response.data.wind.speed
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
-    document.querySelector("#description").innerHTML =
-      response.data.weather[0].main;
+    iconElement.setAttribute("alt", response.data.weather[0].description);
+  
+    getForecast(response.data.coord);
   }
+  
+  
   
   function searchCity(city) {
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
